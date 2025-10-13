7b264f4153656bbdec6f55fc481ad5bc45f1e0f5
Andrew <andreicsgoawp@gmail.com>
2025-10-13 22:05:14 +0300

    Task_2: Added more functions

--- task_2_depersonalization/main.py
+++ task_2_depersonalization/main.py
@@ -2,10 +2,17 @@ from src import depersonalization as dp
 import pandas as pd
 from pathlib import Path
 
-file_path = Path("files") / "dataset_50k.xlsx"
+file_path = Path("files") / "dataset_100k.xlsx"
 df = pd.read_excel(file_path)
-# df = dp.masking_data(df, ['Карта оплаты'], 4, 50)
-df, bins, labels = dp.generalize_costs_quantile(df, 'Стоимость анализов', [0, 0.25, 0.5, 0.75, 1.0])
-dp.detailed_k_anonymity_analysis(df, ['Стоимость анализов'])
 
+# df, bins, labels = dp.generalize_costs_quantile(df, 'Стоимость анализов', [0, 0.25, 0.5, 0.75, 1.0])
+# df = dp.generalize_symptoms(df, 'Симптомы')
+df = dp.microaggregation_of_datas(df, 'Дата посещения врача')
+df = dp.microaggregation_of_datas(df, 'Дата готовности анализов')
+df = dp.masking_data(df, ['Карта оплаты'], 1, 50)
+df = dp.masking_data(df, ['СНИЛС'], 1, 50)
+df = dp.generalize_passport_data(df, 'Паспортные данные')
+df = dp.generalize_symptoms(df, 'Симптомы')
+df = dp.encrypt_name(df, 'Фамилия')
+dp.detailed_k_anonymity_analysis(df, ['Дата посещения врача', 'Симптомы', 'Паспортные данные', 'Карта оплаты', 'Фамилия'])
     
